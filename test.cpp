23ce501589f3f5bab90def769e2386f56c84c3ef
HuyVuong08 <huy.vuongle123@gmail.com>
2019-05-02 23:35:06 +0700

    .csv reader

--- test.cpp
+++ test.cpp
@@ -3,11 +3,45 @@
 
 using namespace std;
 
-int main () 
-{
-    ifstream ip("diabetes.csv");
-    if (!ip.is_open()) 
-        cout << "ERROR: File cannot be opened\n";
-    string ;
-    return 0;
+int main(){
+
+  ifstream ip("diabetes.csv");
+
+  if(!ip.is_open()) std::cout << "ERROR: File Open" << '\n';
+
+  string Pregnancies;
+  string Glucose;
+  string BloodPressure;
+  string SkinThickness;
+  string Insulin;
+  string BMI;
+  string DiabetesPedigreeFunction;
+  string Age;
+  string Outcome;
+
+  while(ip.good()){
+
+    getline(ip,Pregnancies,',');
+    getline(ip,Glucose,',');
+    getline(ip,BloodPressure,',');
+    getline(ip,SkinThickness,',');
+    getline(ip,Insulin,',');
+    getline(ip,BMI,',');
+    getline(ip,DiabetesPedigreeFunction,',');
+    getline(ip,Age,',');
+    getline(ip,Outcome,'\n');
+
+    cout << "Pregnancies: " << Pregnancies << '\n';
+    cout << "Glucose: " << Glucose << '\n';
+    cout << "BloodPressure: " << BloodPressure << '\n';
+    cout << "SkinThickness: " << SkinThickness << '\n';
+    cout << "Insulin: " << Insulin << '\n';
+    cout << "BMI: " << BMI << '\n';
+    cout << "DiabetesPedigreeFunction: " << DiabetesPedigreeFunction << '\n';
+    cout << "Age: " << Age << '\n';
+    cout << "Outcome: " << Outcome << '\n';
+    cout << "--------------------------------" << '\n';
+  }
+
+  ip.close();
 }
