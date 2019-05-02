0ac9dc8228b0ecc86238f4624d2c5bbdc1a0a0db
HuyVuong08 <huy.vuongle123@gmail.com>
2019-05-02 20:15:13 +0700

    Assignment set up

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main () 
+{
+    ifstream ip("diabetes.csv");
+    if (!ip.is_open()) 
+        cout << "ERROR: File cannot be opened\n";
+    string ;
+    return 0;
+}
