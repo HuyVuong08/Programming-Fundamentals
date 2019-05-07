cd6fc783aa4c2dc4b85024c759dbc9fe881c9598
HuyVuong08 <huy.vuongle123@gmail.com>
2019-05-08 00:23:01 +0700

    Struct Record datatype

--- test.cpp
+++ test.cpp
@@ -4,25 +4,42 @@
 using namespace std;
 
 
-/*
+
 struct Record {
     // TODO: Please define the detail struct for storing a record here
+    float Pregnancies [999];
+    float Glucose [999];
+    float BloodPressure [999];
+    float SkinThickness [999];
+    float Insulin [999];
+    float BMI [999];
+    float DiabetesPedigreeFunction [999];
+    float Age [999];
+    float Outcome [999];
     int count = 0;
-    float * pData = new float[count][9];
-    
 };
 
-void LoadData(const char*, void* &)
+void CountLine (const char* FileName, int & count)
 {
+    count = 0;
+    string line;
 
+    ifstream file(FileName);
+
+    while (getline(file, line))
+        count++;
+    file.close();
 }
-*/
-int main(){
 
-    int i = 0;
-    float Data[2][9];    
+void LoadData(const char* pFName, void* & pData)
+{
+    int i = 0, count = 0;
 
-    ifstream ip("diabetes.csv");
+    CountLine (pFName, count);
+    Record Data;
+    Data.count = count;
+
+    ifstream ip(pFName);
 
     if(!ip.is_open()) std::cout << "ERROR: File Open" << '\n';
 
@@ -58,48 +75,29 @@ int main(){
         float Age_float = stof(Age_string);
         float Outcome_float = stof(Outcome_string);
 
-        Data[i][0] = Pregnancies_float;
-        Data[i][1] = Glucose_float;
-        Data[i][2] = BloodPressure_float;    
-        Data[i][3] = SkinThickness_float;    
-        Data[i][4] = Insulin_float;    
-        Data[i][5] = BMI_float;    
-        Data[i][6] = DiabetesPedigreeFunction_float;    
-        Data[i][7] = Age_float;  
-        Data[i][8] = Outcome_float;  
+        Data.Pregnancies[i] = Pregnancies_float;
+        Data.Glucose[i] = Glucose_float;
+        Data.BloodPressure[i] = BloodPressure_float;    
+        Data.SkinThickness[i] = SkinThickness_float;    
+        Data.Insulin[i] = Insulin_float;    
+        Data.BMI[i] = BMI_float;    
+        Data.DiabetesPedigreeFunction[i] = DiabetesPedigreeFunction_float;    
+        Data.Age[i] = Age_float;  
+        Data.Outcome[i] = Outcome_float;  
         
         i ++;
     }
-/*
-    for (int i = 0; i < 2; i ++)
-    {
-        for (int j = 0 ; j < 9; j ++)
-        {
-            if (j = 0)
-                cout << "Pregnancies: " << Data[i][j];
-            else if (j = 1)
-                cout << "Glucose: " << Data[i][j];
-            else if (j = 2)
-                cout << "BloodPressure: " << Data[i][j];
-            else if (j = 3)
-                cout << "SkinThickness: " << Data[i][j];
-            else if (j = 4)
-                cout << "Insulin: " << Data[i][j];
-            else if (j = 5)
-                cout << "BMI: " << Data[i][j];
-            else if (j = 6)
-                cout << "DiabetesPedigreeFunction: " << Data[i][j];
-            else if (j = 7)
-                cout << "Age: " << Data[i][j];
-            else if (j = 8)
-                cout << "Outcome: " << Data[i][j];
-
-            cout << Data[i][j] << '\n';
-        }
-        cout << "--------------------------------" << '\n';
-    }
-*/
     ip.close();
+    pData = &Data;
+}
 
-    cout << Data[1][1];
+int main(){
+    void * pData;
+
+    LoadData ("diabetes.csv", pData); 
+    Record * a;
+    a = (struct Record *) pData; 
+    cout << (*a).Glucose[1];
+
+    return 0;
 }
